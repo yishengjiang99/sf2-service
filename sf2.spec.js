377df3a6c34fa0d2f490d739cd480ad87b1463ec
Yisheng Jiang <yishengjiang@yahoo.com>
2021-07-17 01:56:20 -0700

    df

--- sf2.spec.js
+++ sf2.spec.js
@@ -17,27 +17,30 @@ describe("pdtaquery", () => {
 	})
 });
 describe("pdtaquery", () => {
-	it("parswes file and loads zones",()=>{
-		load("file.sf2").then(({zoneSampleHeaders,setProgram,noteOn, zref2Zone})=>{
-			const sf=	setProgram(0,2);
-			const iter=zoneSampleHeaders(sf);
-			for(const sh of iter){
-				expect(sh.range).exists;
-				fetch("file.sf2",{headers:{Range:sh.range}}).then(res=>res.arrayBuffer()).then(ab=>{
-					const b16=new Int16Array(ab)
-					const f32 = new Float32Array(ab.byteLength/2);
-					let i=0;
-					for(let i=0;i<b16.length; i++){//} of b16){
-						f32[i]=b16[i]/0xffff;
-					}
-					chart(mkcanvas(330,420, document.body), f32);
-				})
-			}
-			const zref = noteOn(2, 45, 22);
-
-		
-		});
-	}).slow(100)
+	it("parswes file and loads zones", () => {
+    load("file.sf2").then(
+      ({ zoneSampleHeaders, setProgram, noteOn, zref2Zone }) => {
+        const sf = setProgram(0, 2);
+        const iter = zoneSampleHeaders(sf);
+        for (const sh of iter) {
+          expect(sh.range).exists;
+          fetch("file.sf2", { headers: { Range: sh.range } })
+            .then((res) => res.arrayBuffer())
+            .then((ab) => {
+              const b16 = new Int16Array(ab);
+              const f32 = new Float32Array(ab.byteLength / 2);
+              let i = 0;
+              for (let i = 0; i < b16.length; i++) {
+                //} of b16){
+                f32[i] = b16[i] / 0xffff;
+              }
+              chart(mkcanvas(330, 420, document.body), f32);
+            });
+        }
+        const zref = noteOn(2, 45, 22);
+      }
+    );
+  });
 });
 
 mocha.run();
